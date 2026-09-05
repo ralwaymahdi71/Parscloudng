1a8981462f3b5088622da3453861305cbbb5668b
ralwaymahdi71 <ralwaymahdi71@atomicmail.io>
2026-09-05 16:50:04 +0330

    chore: rename app package and APK outputs to Parscloud

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = 37
 
     defaultConfig {
-        applicationId = "com.patterniha.pattng"
+        applicationId = "com.parscloud.vpn"
         minSdk = 24
         targetSdk = 37
         versionCode = 746
@@ -92,7 +92,7 @@ android {
                 .map { it as com.android.build.gradle.internal.api.ApkVariantOutputImpl }
                 .forEach { output ->
                     val abi = output.getFilter("ABI") ?: "universal"
-                    output.outputFileName = "PattNG_${variant.versionName}-fdroid_${abi}.apk"
+                    output.outputFileName = "Parscloud_${variant.versionName}-fdroid_${abi}.apk"
                     if (versionCodes.containsKey(abi)) {
                         output.versionCodeOverride =
                             (100 * variant.versionCode + versionCodes[abi]!!).plus(5000000)
@@ -112,7 +112,7 @@ android {
                     else
                         "universal"
 
-                    output.outputFileName = "PattNG_${variant.versionName}_${abi}.apk"
+                    output.outputFileName = "Parscloud_${variant.versionName}_${abi}.apk"
                     if (versionCodes.containsKey(abi)) {
                         output.versionCodeOverride =
                             (1000000 * versionCodes[abi]!!).plus(variant.versionCode)
